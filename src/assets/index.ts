1f56a44e937f2cb2b5d5294e41d043f820d5e293
ztoc <bhu0444@gmail.com>
2025-01-31 05:20:19 -0500

    feat: fix build errors

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -7,8 +7,8 @@ import github from './github.png';
 import menu from './menu.svg';
 import close from './close.svg';
 
-import css from './tech/css.svg';
 import docker from './tech/docker.svg';
+import css from './tech/css.svg';
 import figma from './tech/figma.svg';
 import git from './tech/git.svg';
 import html from './tech/html.svg';
--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -1,38 +1,38 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
+// import React, { Suspense } from 'react';
+// import { Canvas } from '@react-three/fiber';
+// import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
 
-import CanvasLoader from '../layout/Loader';
-import { div } from 'three/tsl';
+// import CanvasLoader from '../layout/Loader';
+// import { div } from 'three/tsl';
 
-const Ball = (props: any) => {
-  const [decal] = useTexture([props.imgUrl]);
+// const Ball = (props: any) => {
+//   const [decal] = useTexture([props.imgUrl]);
 
-  // Check if imgUrl is valid
-  if (!props.imgUrl) {
-    return null; // or return a placeholder
-  }
+//   // Check if imgUrl is valid
+//   if (!props.imgUrl) {
+//     return null; // or return a placeholder
+//   }
 
-  return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          // @ts-expect-error
-          flatShading
-        />
-      </mesh>
-    </Float>
-  );
-};
+//   return (
+//     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+//       <ambientLight intensity={0.25} />
+//       <directionalLight position={[0, 0, 0.05]} />
+//       <directionalLight position={[0, 0, 0.05]} />
+//       <mesh castShadow receiveShadow scale={2.75}>
+//         <icosahedronGeometry args={[1, 1]} />
+//         <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
+//         <Decal
+//           position={[0, 0, 1]}
+//           rotation={[2 * Math.PI, 0, 6.25]}
+//           scale={1}
+//           map={decal}
+//           // @ts-expect-error
+//           flatShading
+//         />
+//       </mesh>
+//     </Float>
+//   );
+// };
 
 // const BallCanvas: React.FC<{ icon: string }> = ({ icon }) => {
 //   return (
